cfa50ed79013b9a4a1f6985847fd063ea4694051
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2025-09-01 14:44:44 +0500

    feat: update database connection string, add authentication package, and create user schema migration

---
+++ PostgreSQL/gemStone/prisma/migrations/20250829094930_create_user_schema/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" UUID NOT NULL DEFAULT 'gen_random_uuid()',
+    "name" VARCHAR(100) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
